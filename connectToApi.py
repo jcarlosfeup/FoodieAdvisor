b35fc0b96711775515bfa937516ff3b740831dde
jcarlosportela <jcarlosportelapereira@gmail.com>
2024-12-31 19:33:43 +0000

    Obtained first API response with success

--- connectToApi.py
+++ connectToApi.py
@@ -5,12 +5,12 @@ from google.auth.transport.requests import Request
 
 API_KEY = "AIzaSyBOHqgJq1SLlb7-IIzntJrRwNUX0Wt7Anw"
 BASE_URL = "https://places.googleapis.com/v1/places:searchText"
-SERVICE_ACCOUNT_FILE = "google_client_secret.json"
+SERVICE_ACCOUNT_FILE = "foodie-advisor-e02c03a87c85.json"
 
 
 credentials = service_account.Credentials.from_service_account_file(
     SERVICE_ACCOUNT_FILE,
-    scopes=["https://www.googleapis.com/auth/places"]
+    scopes=["https://www.googleapis.com/auth/cloud-platform"]
 )
 
 
@@ -18,10 +18,12 @@ def get_access_token(credentials):
     credentials.refresh(Request())
     return credentials.token
 
+# places.displayName,places.id TO SELECT specific fields
 def make_api_call(token, search_text: str):
     header = {
         "Authorization": f"Bearer {token}",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "X-Goog-FieldMask": "places.displayName,places.id"
     }
 
     body = {
@@ -39,4 +41,4 @@ if __name__ == "__main__":
     access_token = get_access_token(credentials)
 
     make_api_call(token=access_token,
-                  search_text="Spicy Vegetarian Food in Sydney, Australia")
+                  search_text="Vegetarian Food in Porto, Portugal")
