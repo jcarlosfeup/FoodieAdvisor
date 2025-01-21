858b8563393f471b2536c24f05ee5572fd346fcb
jcarlosportela <jcarlosportelapereira@gmail.com>
2025-01-21 19:23:33 +0000

    Added icon to marker

--- visualization.py
+++ visualization.py
@@ -4,10 +4,14 @@ from storage import ReadWriterCSVHandler, FILENAME, BUCKET_NAME
 from streamlit_folium import st_folium
 
 
+def create_marker_icon(path: str):
+    return folium.CustomIcon(icon_image=path,
+                             icon_size=(30, 30))
+
+
 if __name__ == "__main__":
     bucket_writer = ReadWriterCSVHandler(filename=FILENAME,
                                          bucket_name=BUCKET_NAME)
-    #bucket_writer.upload_dataframe_to_gcs()
     bucket_writer.read_df_from_bucket()
 
     print(bucket_writer.df)
@@ -20,12 +24,13 @@ if __name__ == "__main__":
 
     mapa = folium.Map(location=[filtered_data["latitude"].mean(), filtered_data["longitude"].mean()],
                                 zoom_start=13)
-    
+
     for _, row in filtered_data.iterrows():
         folium.Marker(
             location=[row['latitude'], row['longitude']],
             popup=f"{row['name']} (Rating: {row['rating']})",
-            tooltip=row["name"]
+            tooltip=row["name"],
+            icon=create_marker_icon(path="assets/images/icon.png")
         ).add_to(mapa)
 
     st_folium(mapa,
