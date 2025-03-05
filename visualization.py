78a471af7ae70b4ae0ea03a3f956e0068ad83129
jcarlosportela <jcarlosportelapereira@gmail.com>
2025-03-05 23:57:26 +0000

    Changed text and background aspect

--- visualization.py
+++ visualization.py
@@ -1,8 +1,26 @@
 import streamlit
 import folium
+import base64
 from storage import ReadWriterCSVHandler, FILENAME, BUCKET_NAME
 from streamlit_folium import st_folium
 from input import get_cities_df
+
+
+def add_background_image(path: str):
+    with open(path, "rb") as file:
+        encoded_image = base64.b64encode(file.read()).decode()
+
+    # taken from https://stackoverflow.com/questions/72582550/how-do-i-add-background-image-in-streamlit
+    html_template = """
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    """ % encoded_image
+    streamlit.markdown(html_template,
+                       unsafe_allow_html=True)
 
 
 def create_marker_icon(path: str):
@@ -11,9 +29,11 @@ def create_marker_icon(path: str):
 
 
 def create_selectbox_list(cities: list):
-    city = streamlit.selectbox("Select a city", cities)
-
-
+    city = streamlit.selectbox(label="Where you at?",
+                               options=cities,
+                               index=None,
+                               placeholder="Select city")
+    return city
 
 
 if __name__ == "__main__":
@@ -23,23 +43,32 @@ if __name__ == "__main__":
     print(bucket_writer.df)
 
     city_list = get_cities_df()['name'].unique()
-    
-    streamlit.title("Foodie Advisor - Best Restaurants by location")
+
+    #city_list = list(filter(lambda k: 'Porto' in k and len(k) == 5, city_list))
+
+    add_background_image(path="assets/images/background.jpg")
+
+    streamlit.title("Foodie Advisor")
+    streamlit.markdown("#### Best local cuisine according with your location")
     city = create_selectbox_list(city_list)
-    
+
+    print(f"City is {city}")
     filtered_data = bucket_writer.df[bucket_writer.df['city'] == city]
 
-    mapa = folium.Map(location=[filtered_data["latitude"].mean(), filtered_data["longitude"].mean()],
-                                zoom_start=13)
+    if len(filtered_data.index) > 0:
+        mapa = folium.Map(location=[filtered_data["latitude"].mean(), filtered_data["longitude"].mean()],
+                                    zoom_start=13)
 
-    for _, row in filtered_data.iterrows():
-        folium.Marker(
-            location=[row['latitude'], row['longitude']],
-            popup=f"{row['name']} (Rating: {row['rating']})",
-            tooltip=row["name"],
-            icon=create_marker_icon(path="assets/images/icon.png")
-        ).add_to(mapa)
+        for _, row in filtered_data.iterrows():
+            folium.Marker(
+                location=[row['latitude'], row['longitude']],
+                popup=f"{row['name']} (Rating: {row['rating']})",
+                tooltip=row["name"],
+                icon=create_marker_icon(path="assets/images/icon.png")
+            ).add_to(mapa)
 
-    st_folium(mapa,
-              width=700,
-              height=500)
+        st_folium(mapa,
+                width=700,
+                height=500)
+    else:
+        streamlit.subheader("No results founds in this city!")
